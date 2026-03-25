464ac1172e3eb69540ba736645181c51d9041708
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2026-03-25 18:57:37 +0530

    create populateDB.js to store initial data

---
+++ src/lib/api.ts
@@ -0,0 +1,12 @@
+// global api
+
+export async function api(url: string, options: RequestInit = {}) {
+  const res = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      ...(options.headers || {}),
+    },
+    ...options,
+  });
+  return res;
+}
