4f4f148869503387227924238ade30b8ef351b78
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2026-04-06 11:35:44 +0530

    feat: use custom crosshair cursor onto game image

--- src/components/containers/PlayMode.tsx
+++ src/components/containers/PlayMode.tsx
@@ -3,15 +3,26 @@ import styles from "./PlayMode.module.css";
 
 interface PlayModeProps {
   modeData: Mode | null;
+  mode: string;
 }
 
 const PlayMode = ({ modeData }: PlayModeProps) => {
+  const handleClick = (e: React.MouseEvent<HTMLImageElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+
+    const x = rect.width;
+    const y = rect.height;
+
+    console.log(`Clicked at X:${x}, y:${y}`);
+  };
+
   return (
     <>
       <img
         src={modeData?.modeImageUrl}
         alt={modeData?.description}
         className={styles.img}
+        onClick={handleClick}
       />
     </>
   );
